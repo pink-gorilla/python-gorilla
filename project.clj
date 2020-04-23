e737e03c8c602e962f5c3a3b10cb7f6f72b10131
CI/CD <ci@pinkgorilla.org>
2020-04-23 04:03:48 +0000

    Release 0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/python-gorilla "0.0.1-SNAPSHOT"
+(defproject org.pinkgorilla/python-gorilla "0.0.1"
   :description "PinkGorilla<->Python interop (libpython-clj)"
   :license {:name "MIT"}
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
